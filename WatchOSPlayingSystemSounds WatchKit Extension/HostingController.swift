3096864e0f14e5c884545e2fd2a9fbbb129c567d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-04-21 10:04:39 +0300

    Initial Commit

---
+++ WatchOSPlayingSystemSounds WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchOSPlayingSystemSounds WatchKit Extension
+//
+//  Created by ramil on 21.04.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchOSPlayingSystemSounds WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WatchOSPlayingSystemSounds WatchKit Extension
+//
+//  Created by ramil on 21.04.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
